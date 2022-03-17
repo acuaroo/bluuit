3ddae3260e9624f4999977b8304ef3edd3a66703
Acuaro <67112172+acuaroo@users.noreply.github.com>
2022-03-17 06:41:51 -0600

    added main files

---
+++ js/memes.js
@@ -0,0 +1,56 @@
+var after = "";
+var on = false;
+
+function fetchMemes() {
+  let parentdiv;
+
+  if (document.getElementById("memes")) {
+    parentdiv = document.getElementById("memes")
+  } else {
+    parentdiv = document.createElement("div");
+    parentdiv.id = "memes";
+  }
+
+  
+
+  
+  fetch(`https://www.reddit.com/r/memes.json?after=${after}`)
+    .then((response) => response.json())
+    
+    .then((body) => {
+      after = body.data.after;
+      for (let index = 0; index < body.data.children.length; index++) {
+        if (body.data.children[index].data.post_hint === "image") {
+          let div = document.createElement("div");
+          let a = document.createElement("a")
+          let h4 = document.createElement("h4");
+          let image = document.createElement("img");
+          a.href=body.data.children[index].data.url_overridden_by_dest;
+          image.src = body.data.children[index].data.url_overridden_by_dest;
+          h4.textContent = body.data.children[index].data.title+" | "+body.data.children[index].data.ups+" ⬆️"
+          div.appendChild(a);
+
+          a.appendChild(h4);
+          a.appendChild(image);
+          parentdiv.appendChild(div);
+          on = false;
+
+        }
+      }
+      document.body.appendChild(parentdiv);
+      
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+
+}
+
+window.addEventListener('scroll',()=>{
+    console.log(on);
+    if(window.scrollY + window.innerHeight >= 
+        document.documentElement.scrollHeight + 10 && on === false){
+        on = true;
+        fetchMemes();
+    }
+})
